cf32a83559fd4c3d2a0f0e116189026f86d82066
Alexander Alemayhu <alexander@alemayhu.com>
2017-12-13 05:16:34 +0100

    scripts: try to fix missing clang dependencies
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- scripts/3_get_llvm_snapshot.sh
+++ scripts/3_get_llvm_snapshot.sh
@@ -7,5 +7,7 @@ wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial main" | sudo tee -a /etc/apt/sources.list
 echo "deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial main" | sudo tee -a /etc/apt/sources.list
 
-sudo apt-get update
-sudo apt-get -y install clang-6.0 lldb-6.0 lld-6.0
+sudo apt-get update || true
+sudo apt-get -y install clang-6.0 lldb-6.0 lld-6.0 || true
+
+sudo apt-get install -f
