2c92b38e6628555efd5dda1edab7eb86d8d7ec34
Alexander Alemayhu <alexander@alemayhu.com>
2017-12-24 16:38:24 +0100

    scripts: fix 5_install_bpftool to fail properly
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- scripts/5_install_bpftool.sh
+++ scripts/5_install_bpftool.sh
@@ -4,7 +4,13 @@ set -xe
 
 KDIR=$1 # Kernel tree
 TEST_DIR=$KDIR/tools/testing/selftests/bpf/
+BPFTOOL_DIR=$KDIR/tools/bpf/bpftool/
 
-set +e
-(cd $KDIR/tools/bpf/bpftool/ && make && sudo make install) || true
-set -e
+if ! test -d $BPFTOOL_DIR; then
+  echo "XXX: could not find bpftool, maybe too old kernel?"
+  exit 0
+fi
+
+cd $BPFTOOL_DIR
+make
+sudo make install
