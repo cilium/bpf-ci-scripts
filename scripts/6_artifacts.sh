9f2f9e7689a70767475a8c2310e179fad697f638
Alexander Alemayhu <alexander@alemayhu.com>
2017-12-25 10:00:15 +0100

    remove broken results file
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- scripts/6_artifacts.sh
+++ scripts/6_artifacts.sh
@@ -5,3 +5,4 @@ set -x
 mkdir -pv ARTIFACTS
 vagrant scp :workspace/tools/testing/selftests/bpf/ ARTIFACTS/selftest-bpf || true
 vagrant scp :workspace/.config ARTIFACTS/KernelConfig.txt || true
+vagrant scp :/tmp/*_result.txt ARTIFACTS/ || true
