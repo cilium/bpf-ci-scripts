d6dc1cbe2921daa925e01444c217b7bda15b92c7
Alexander Alemayhu <alexander@alemayhu.com>
2017-12-30 14:10:40 +0100

    scripts: create empty results file
    Should prevent email failure if selftest output is never reached or
    written.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- scripts/6_artifacts.sh
+++ scripts/6_artifacts.sh
@@ -6,3 +6,4 @@ mkdir -pv ARTIFACTS
 vagrant scp :workspace/tools/testing/selftests/bpf/ ARTIFACTS/selftest-bpf || true
 vagrant scp :workspace/.config ARTIFACTS/KernelConfig.txt || true
 vagrant scp :/tmp/*_result.txt ARTIFACTS/ || true
+touch ARTIFACTS/empty_result.txt
