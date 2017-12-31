6c33e6cf0335a35915debb48da6d6abf152ebcd0
Alexander Alemayhu <alexander@alemayhu.com>
2017-12-31 18:17:19 +0100

    scripts: delete the ARTIFACTS directory before copying
    Removing the artifacts directory should prevent sending outdated
    results in the email notification.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- scripts/6_artifacts.sh
+++ scripts/6_artifacts.sh
@@ -2,6 +2,7 @@
 
 set -x
 
+rm -rvf ARTIFACTS || true
 mkdir -pv ARTIFACTS
 vagrant scp :workspace/tools/testing/selftests/bpf/ ARTIFACTS/selftest-bpf || true
 vagrant scp :workspace/.config ARTIFACTS/KernelConfig.txt || true
