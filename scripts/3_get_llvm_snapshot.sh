80ad0905212bb7fc0064b5134c4afc7308524154
Alexander Alemayhu <alexander@alemayhu.com>
2018-01-08 11:39:59 +0100

    scripts: try to deal with LLVM repository issues
    Sometimes the build fails because the LLVM repository seems to be
    unavailable. Change the script to try multiple times before continuing
    Also do a final version check, this should make the Jenkins stage fail
    if the installation was unsuccessful.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- scripts/3_get_llvm_snapshot.sh
+++ scripts/3_get_llvm_snapshot.sh
@@ -2,12 +2,23 @@
 
 set -xe
 
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+RETRY=180s
+
+for i in $(seq 1 3); do
+  wget -P /tmp/ https://apt.llvm.org/llvm-snapshot.gpg.key || (sleep $RETRY && continue)
+  break
+done
+
+sudo apt-key add /tmp/llvm-snapshot.gpg.key
 
 echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial main" | sudo tee -a /etc/apt/sources.list
 echo "deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial main" | sudo tee -a /etc/apt/sources.list
 
-sudo apt-get update || true
-sudo apt-get -y install clang-6.0 lldb-6.0 lld-6.0 || true
+for i in $(seq 1 3); do
+  sudo apt-get update || (sleep $RETRY && continue)
+  sudo apt-get -y install clang-6.0 lldb-6.0 lld-6.0 || (sleep $RETRY && continue)
+  sudo apt-get install -f -y
+done
 
-sudo apt-get install -f -y
+/usr/bin/llc-6.0 --version
+/usr/bin/clang-6.0 --version
