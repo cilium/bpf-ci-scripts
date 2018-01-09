42b671fd0133246a3dd475c13d081c9afd175ec7
Alexander Alemayhu <alexander@alemayhu.com>
2018-01-09 08:18:38 +0100

    scripts: try sleeping more for networking issues with LLVM repository
    The flake still occcurred even after the delay. From eyeballing the
    logs, adding more delay in the loop might resolve this. Will know
    tomorrow or later today.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- scripts/3_get_llvm_snapshot.sh
+++ scripts/3_get_llvm_snapshot.sh
@@ -17,7 +17,9 @@ echo "deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial main" | sudo tee
 for i in $(seq 1 3); do
   sudo apt-get update || (sleep $RETRY && continue)
   sudo apt-get -y install clang-6.0 lldb-6.0 lld-6.0 || (sleep $RETRY && continue)
-  sudo apt-get install -f -y
+  sudo apt-get install -f -y || (sleep $RETRY && continue)
+  /usr/bin/llc-6.0 --version || (sleep $RETRY && continue)
+  /usr/bin/clang-6.0 --version || (sleep $RETRY && continue)
 done
 
 /usr/bin/llc-6.0 --version
